5d44c543b293c4518a37e7926b99c7ae1e6acc06
SamuelBates <47985337+SamuelBates@users.noreply.github.com>
2019-02-27 11:22:51 -0500

    Add files via upload

---
+++ Enemy.java
@@ -0,0 +1,16 @@
+/* Author: Samuel Bates
+ * Date: 2-27-2019
+ * Purpose: To have a class used entirely for skills of the enemy and player
+ */
+package Unit6_Task;
+
+public class Enemy {
+	
+	String Name;
+	int Health;
+	int Damage;
+	Skill Skill1 = new Skill();
+	Skill Skill2 = new Skill();
+	Skill Skill3 = new Skill();
+
+}
---
+++ Skill.java
@@ -0,0 +1,12 @@
+/* Author: Samuel Bates
+ * Date: 2-27-2019
+ * Purpose: To have a class used entirely for skills of the enemy and player
+ */
+package Unit6_Task;
+
+public class Skill {
+	String name;
+	public Skill() {
+		this.name = "";
+	}
+}
