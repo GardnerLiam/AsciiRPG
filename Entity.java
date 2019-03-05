1c87d8996006f6b515c07b356ead96291961db17
SamuelBates <47985337+SamuelBates@users.noreply.github.com>
2019-03-05 15:03:15 -0500

    Add files via upload

---
+++ Entity.java
@@ -0,0 +1,17 @@
+/* Author: Samuel Bates
+ * Date: 2-27-2019
+ * Purpose: To have a class used entirely for skills of the enemy and player
+ */
+package Entity;
+
+public class Entity {
+	
+	String Name;
+	int Health;
+	int Damage;
+	int X;
+	int Y;
+	Skill Skill1 = new Skill();
+	Skill Skill2 = new Skill();
+	Skill Skill3 = new Skill();
+}
